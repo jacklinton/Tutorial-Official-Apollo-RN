c65eff04fb19ca8616ccb2f61f2f1a9395826a51
Jack Linton <jacklinton@mac.com>
2017-04-11 23:49:35 -0400

    Completed first exercise.

--- exercise-01/App.js
+++ exercise-01/App.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import { Actions, Scene, Router } from 'react-native-router-flux'
+import React from 'react';
+import { Actions, Scene, Router } from 'react-native-router-flux';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
-import Pokedex from './components/Pokedex'
+import Pokedex from './components/Pokedex';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/__PROJECT_ID__'}),
+});
 
 const scenes = Actions.create(
   <Scene key='root'>
     <Scene key='pokedex' component={Pokedex} title='Pokedex' initial={true} type='replace' />
   </Scene>
-)
+);
+
+
 
 export default class App extends React.Component {
   render() {
     return (
-        <Router scenes={scenes}/>
+    	<ApolloProvider client={client}>
+        	<Router scenes={scenes}/>
+    	</ApolloProvider>
     )
   }
-}
+};
